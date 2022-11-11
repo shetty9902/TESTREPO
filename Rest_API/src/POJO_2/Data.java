f4e6e8445bb8a5df8cbff03e1649bacc4e369d7f
Mahantesh <Mahantesh@LAPTOP-HBB6PKK1>
2022-11-11 13:27:37 +0530

    for test

---
+++ Rest_API/src/POJO_2/Data.java
@@ -0,0 +1,28 @@
+package POJO_2;
+
+public class Data 
+{
+	private int id;
+	private String name;
+	private String year;
+	private String color;
+	private String pantone_value;
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getYear() {
+		return year;
+	}
+	public String getColor() {
+		return color;
+	}
+	public String getPantone_value() {
+		return pantone_value;
+	}
+	
+	
+
+}
